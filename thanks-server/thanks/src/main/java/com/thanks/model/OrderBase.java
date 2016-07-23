a77769d7c76f1fff2a2c31e30b5c9a621bcbca06
Micky Kim <ksmail13@me.com>
2016-07-23 18:09:19 +0900

    no message

---
+++ thanks-server/thanks/src/main/java/com/thanks/model/OrderBase.java
@@ -0,0 +1,14 @@
+package com.thanks.model;
+
+import lombok.Data;
+
+import javax.persistence.MappedSuperclass;
+
+/**
+ * Created by micky on 2016. 7. 23..
+ */
+@Data
+@MappedSuperclass
+public class OrderBase {
+
+}
