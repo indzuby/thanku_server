c46c762a79f5e1eb42107d1b68827f8a3821dcfc
zuby <user1@user1ui-MacBook-Pro.local>
2016-07-23 23:30:27 +0900

    rider 추가

--- thanks-server/thanks/src/main/java/com/thanks/controller/RiderController.java
+++ thanks-server/thanks/src/main/java/com/thanks/controller/RiderController.java
@@ -3,6 +3,7 @@ package com.thanks.controller;
 import com.thanks.form.EmailSignUpForm;
 import com.thanks.model.User;
 import com.thanks.service.UserService;
+import com.thanks.util.annotation.CurrentUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -22,4 +23,19 @@ public class RiderController {
     public User addRiderByEmail(@RequestBody EmailSignUpForm user) {
         return userService.add(user.toRider());
     }
+
+
+    @RequestMapping(method = RequestMethod.GET, value="")
+    @ResponseBody
+    public User getMyInfo(@CurrentUser User user) {
+        return user;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value="/update")
+    @ResponseBody
+    public User update(@RequestBody User user) {
+
+        return userService.update(user.getId(),user);
+    }
+
 }
